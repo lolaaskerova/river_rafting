5c44b6ff1d776b5882a9799ac7ff9200627e4322
lolaaskerova <lolaaskrova2@gmail.com>
2023-02-09 22:45:02 +0400

    Tours page: hero section is done

---
+++ frontend/src/components/ToursComponents/ToursHeroSection/ToursHeroSection.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./toursHeroSection.scss";
+const ToursHeroSection = () => {
+  return (
+    <section className="tours-hero">
+      <div className="tours-hero-title">
+        <h1>OUR TOURS</h1>
+      </div>
+    </section>
+  );
+};
+
+export default ToursHeroSection;
