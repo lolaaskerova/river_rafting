e4b8239b04892eb6efb1d1fddcbcdfd014edd33b
lolaaskerova <lolaaskrova2@gmail.com>
2023-02-09 18:21:15 +0400

    home page ready

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,15 +1,20 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import HeroSection from "../components/HomeComponents/HeroSection/HeroSection";
 import HomeAbout from "../components/HomeComponents/HomeAbout/HomeAbout";
 import HomeAllTours from "../components/HomeComponents/HomeAllTours/HomeAllTours";
 import Chilko from "../components/HomeComponents/HomeChilko/Chilko";
 import HomeFeedBack from "../components/HomeComponents/HomeFeedBack/HomeFeedBack";
+import HomeLocation from "../components/HomeComponents/HomeLocation/HomeLocation";
 import HomePackages from "../components/HomeComponents/HomePackages/HomePackages";
 import HomeTour from "../components/HomeComponents/HomeTours/HomeTour";
 
 const Home = () => {
   return (
     <>
+      <Helmet>
+        <title>Home</title>
+      </Helmet>
       <HeroSection />
       <HomeAbout />
       <HomeAllTours />
@@ -17,6 +22,7 @@ const Home = () => {
       <Chilko />
       <HomeFeedBack />
       <HomePackages />
+      <HomeLocation />
     </>
   );
 };
