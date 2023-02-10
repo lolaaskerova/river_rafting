9c2798a4f35800803a41d588baee3ea2a3e80a6d
lolaaskerova <lolaaskrova2@gmail.com>
2023-02-10 18:33:34 +0400

    About Us: hero section done

---
+++ frontend/src/components/AboutUsComponents/AboutUsHero/AboutUsHero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./aboutUsHero.scss";
+const AboutUsHero = () => {
+  return (
+    <section className="about-us-hero">
+      <div className="about-us-hero-title">
+        <h1>ABOUT US</h1>
+      </div>
+    </section>
+  );
+};
+
+export default AboutUsHero;
--- frontend/src/pages/AboutUs.jsx
+++ frontend/src/pages/AboutUs.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import AboutUsHero from "../components/AboutUsComponents/AboutUsHero/AboutUsHero";
 const AboutUs = () => {
   return (
     <>
       <Helmet>
         <title>About Us - River Rafting</title>
       </Helmet>
-      AboutUs
+      <AboutUsHero />
     </>
   );
 };
