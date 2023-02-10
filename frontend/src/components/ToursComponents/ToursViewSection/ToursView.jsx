4a7004dbc2409febbcfe8b1338937dcbdd2efbc1
lolaaskerova <lolaaskrova2@gmail.com>
2023-02-10 16:14:41 +0400

    Tours page: some changes added

--- frontend/src/components/ToursComponents/ToursViewSection/ToursView.jsx
+++ frontend/src/components/ToursComponents/ToursViewSection/ToursView.jsx
@@ -3,20 +3,26 @@ import "./toursView.scss";
 import { AiOutlineArrowRight } from "react-icons/ai";
 const ToursView = () => {
   return (
-    <div className="tours-view">
-      <div className="tours-view-inner container">
-        <div className="tours-view-card">
-          <h3>Save 25%</h3>
-          <p>
-            Come and join the thrill and excitement of white water rafting with
-            the adventure rafting team!
-          </p>
-          <a href="">
-            VIEW TOUR SPECIALS <AiOutlineArrowRight />{" "}
-          </a>
+    <>
+      <div className="tours-view">
+        <div className="tours-view-inner container">
+          <div className="tours-view-card">
+            <h3>Save 25%</h3>
+            <p>
+              Come and join the thrill and excitement of white water rafting
+              with the adventure rafting team!
+            </p>
+            <a href="">
+              VIEW TOUR SPECIALS <AiOutlineArrowRight />{" "}
+            </a>
+          </div>
         </div>
       </div>
-    </div>
+      <div className="tour_link">
+        <h4>Download our waiver forms here</h4>
+        <button>Download Forms</button>
+      </div>
+    </>
   );
 };
 
