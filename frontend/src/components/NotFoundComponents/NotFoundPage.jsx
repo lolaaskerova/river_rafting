836c96850e2bfc9738065556162b9aff2e3a256e
lolaaskerova <lolaaskrova2@gmail.com>
2023-02-11 12:42:05 +0400

    Not Foung page done

--- frontend/src/components/NotFoundComponents/NotFoundPage.jsx
+++ frontend/src/components/NotFoundComponents/NotFoundPage.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 import "./notFoundPage.scss";
 const NotFoundPage = () => {
-  return <div>xx</div>;
+  return (
+    <div className="not-found-field">
+      <div className="container">
+        <div className="not-found-title">
+          <h1>PAGE NOT FOUND</h1>
+        </div>
+        <div className="not-found-input-part">
+          <p>Sorry, the page you are looking for has not been found.</p>
+          <div className="not-found-input">
+            <input type="search" placeholder="Search River Rafting" />
+            <button>Search</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NotFoundPage;
