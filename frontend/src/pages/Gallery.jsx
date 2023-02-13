fac20b7517c7e4804dd61e0f7e69bbecb7688da6
lolaaskerova <lolaaskrova2@gmail.com>
2023-02-13 13:04:48 +0400

    Gallery page done

--- frontend/src/pages/Gallery.jsx
+++ frontend/src/pages/Gallery.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import GalleryFirstGrid from "../components/GalleryComponents/GalleryFirstGrid/GalleryFirstGrid";
+import GalleryHero from "../components/GalleryComponents/GalleryHero/GalleryHero";
+import GalleryLink from "../components/GalleryComponents/GalleryLink/GalleryLink";
+import GallerySecondGrid from "../components/GalleryComponents/GallerySecondGrid/GallerySecondGrid";
 const Gallery = () => {
   return (
     <>
       <Helmet>
         <title>Gallery - River Rafting</title>
       </Helmet>
+      <GalleryHero />
+      <GalleryFirstGrid />
+      <GalleryLink />
+      <GallerySecondGrid />
     </>
   );
 };
