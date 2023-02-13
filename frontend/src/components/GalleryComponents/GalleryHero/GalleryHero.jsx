26ed8696bb5c35c2af7f102e625f8fc2a0c938ba
lolaaskerova <lolaaskrova2@gmail.com>
2023-02-13 10:56:16 +0400

    Gallery page: Hero section done

---
+++ frontend/src/components/GalleryComponents/GalleryHero/GalleryHero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./galleryHero.scss";
+const GalleryHero = () => {
+  return (
+    <div className="gallery-hero">
+      <div className="gallery-hero-title">
+        <h1>GALLERY</h1>
+      </div>
+    </div>
+  );
+};
+
+export default GalleryHero;
