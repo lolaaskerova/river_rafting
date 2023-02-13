86487b9fae575390a7f72baa6dd36f6c2448ca58
lolaaskerova <lolaaskrova2@gmail.com>
2023-02-13 16:23:59 +0400

    FAQ: hero section updated

---
+++ frontend/src/components/FAQComponents/FAQHero/FAQHero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./faqHero.scss";
+const FAQHero = () => {
+  return (
+    <section className="faq-hero">
+      <div className="faq-hero-title">
+        <h1>FREQUENTLY ASKED QUESTIONS</h1>
+      </div>
+    </section>
+  );
+};
+
+export default FAQHero;
