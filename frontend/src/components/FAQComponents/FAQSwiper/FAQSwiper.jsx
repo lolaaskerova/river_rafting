b07022f022e6a28372dc2e0f31d5bb325476a97b
lolaaskerova <lolaaskrova2@gmail.com>
2023-02-13 16:43:39 +0400

    FAQ page: swiper section done

---
+++ frontend/src/components/FAQComponents/FAQSwiper/FAQSwiper.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import SwiperSlider from "../../SwiperSlider/SwiperSlider";
+import swiper1 from "../../../assets/images/firstmodal3.jpg";
+import swiper2 from "../../../assets/images/firstmodal1.jpg";
+import swiper3 from "../../../assets/images/firstmodal2.jpg";
+
+const FAQSwiper = () => {
+  return <SwiperSlider img1={swiper1} img2={swiper2} img3={swiper3} />;
+};
+
+export default FAQSwiper;
