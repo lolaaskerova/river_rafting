ae3e8ce29f1fec895f5a0d4504afb9035b17c179
lolaaskerova <lolaaskrova2@gmail.com>
2023-02-13 15:45:58 +0400

    Packages page : info section updated

--- frontend/src/components/PackagesComponents/PackageInfo/PackageInfo.jsx
+++ frontend/src/components/PackagesComponents/PackageInfo/PackageInfo.jsx
@@ -3,37 +3,26 @@ import "./packageInfo.scss";
 import bag from "../../../assets/images/bag.png";
 import card from "../../../assets/images/card.png";
 import bigbag from "../../../assets/images/bigbag.png";
+import InfoContent from "../../InfoContent/InfoContent";
 const PackageInfo = () => {
   return (
     <div className="package-info container">
       <div className="package-info-field">
-        <div className="package-info-content">
-          <div className="icon">
-            <img src={bigbag} alt="package-info-content" />
-          </div>
-          <div className="package-info-content-titles">
-            <h4>Packages</h4>
-            <p>All inclusive bundles and packages</p>
-          </div>
-        </div>
-        <div className="package-info-content">
-          <div className="icon">
-            <img src={card} alt="package-info-content" />
-          </div>
-          <div className="package-info-content-titles">
-            <h4>Specials</h4>
-            <p>Great deals on amazing tours</p>
-          </div>
-        </div>
-        <div className="package-info-content">
-          <div className="icon">
-            <img src={bag} alt="package-info-content" />
-          </div>
-          <div className="package-info-content-titles">
-            <h4>Retreats</h4>
-            <p>Large groups and special events</p>
-          </div>
-        </div>
+        <InfoContent
+          img={bigbag}
+          title={"Packages"}
+          text={"All inclusive bundles and packages"}
+        />
+        <InfoContent
+          img={card}
+          title={"Specials"}
+          text={"Great deals on amazing tours"}
+        />
+        <InfoContent
+          img={bag}
+          title={"Retreats"}
+          text={"Large groups and special events"}
+        />
       </div>
     </div>
   );
