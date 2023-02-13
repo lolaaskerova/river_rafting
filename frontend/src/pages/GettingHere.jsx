1e9c27d707fb956b00ef45e23a241c90880e26c8
lolaaskerova <lolaaskrova2@gmail.com>
2023-02-13 22:49:44 +0400

    Getting Here page done

--- frontend/src/pages/GettingHere.jsx
+++ frontend/src/pages/GettingHere.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import GetHereDirections from "../components/GettingHereComponents/GetHereDirections/GetHereDirections";
+import GetHerExperience from "../components/GettingHereComponents/GetHereExperience/GetHerExperience";
+import GetHereHero from "../components/GettingHereComponents/GetHereHero/GetHereHero";
+import GetHereLink from "../components/GettingHereComponents/GetHereLink/GetHereLink";
+import GetHereShuttle from "../components/GettingHereComponents/GetHereShuttle/GetHereShuttle";
+import GetHereSwiper from "../components/GettingHereComponents/GetHereSwiper/GetHereSwiper";
+import Location from "../components/Location/Location";
 const GettingHere = () => {
   return (
     <>
       <Helmet>
         <title>Getting Here - River Rafting</title>
       </Helmet>
+      <GetHereHero />
+      <GetHerExperience />
+      <GetHereSwiper />
+      <GetHereDirections />
+      <GetHereLink />
+      <GetHereShuttle />
+      <Location />
     </>
   );
 };
