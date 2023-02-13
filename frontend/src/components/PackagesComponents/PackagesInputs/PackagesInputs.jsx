4b809146a24513baecfa260a0b839231542b9137
lolaaskerova <lolaaskrova2@gmail.com>
2023-02-13 10:45:19 +0400

    Packages page: Input part done

---
+++ frontend/src/components/PackagesComponents/PackagesInputs/PackagesInputs.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import QuestionInputs from "../../QuestionInputs/QuestionInputs";
+import "./packagesInputs.scss";
+
+const PackagesInputs = () => {
+  return (
+    <>
+      <div className="get-in-touch-title">
+        <h1>GET IN TOUCH</h1>
+      </div>
+      <QuestionInputs className="paackages-inputs" />
+    </>
+  );
+};
+
+export default PackagesInputs;
--- frontend/src/pages/PackagesAndSpecials.jsx
+++ frontend/src/pages/PackagesAndSpecials.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Helmet } from "react-helmet";
 import PackageHero from "../components/PackagesComponents/PackageHero/PackageHero";
 import PackageInfo from "../components/PackagesComponents/PackageInfo/PackageInfo";
+import PackagesInputs from "../components/PackagesComponents/PackagesInputs/PackagesInputs";
 import PackagesPackage from "../components/PackagesComponents/PackagesPackageSection/PackagesPackage";
 import PackagesRetreats from "../components/PackagesComponents/PackagesRetreats/PackagesRetreats";
 import PackagesSpecials from "../components/PackagesComponents/PackagesSpecials/PackagesSpecials";
@@ -17,6 +18,7 @@ const PackagesAndSpecials = () => {
       <PackagesPackage />
       <PackagesSpecials />
       <PackagesRetreats />
+      <PackagesInputs />
     </>
   );
 };
