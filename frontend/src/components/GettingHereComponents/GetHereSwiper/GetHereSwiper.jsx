1571a01d0bfd5ec38550f0639e43d295bdda7af0
lolaaskerova <lolaaskrova2@gmail.com>
2023-02-13 19:28:34 +0400

    Getting Here page: swiper done

---
+++ frontend/src/components/GettingHereComponents/GetHereSwiper/GetHereSwiper.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import swiper1 from "../../../assets/images/gethereSwiper2.jpg";
+import swiper2 from "../../../assets/images/gethereSwiper3.jpg";
+import mountain from "../../../assets/images/mountain.jpg";
+import SwiperSlider from "../../SwiperSlider/SwiperSlider";
+const GetHereSwiper = () => {
+  return <SwiperSlider img1={swiper1} img2={swiper2} img3={mountain} />;
+};
+
+export default GetHereSwiper;
