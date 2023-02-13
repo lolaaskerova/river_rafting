d8b27bc3cc6da9e9ae2cec24ebe1f8d2b23565af
lolaaskerova <lolaaskrova2@gmail.com>
2023-02-13 19:08:45 +0400

    Getting Here page: hero section done

---
+++ frontend/src/components/GettingHereComponents/GetHereHero/GetHereHero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./getHereHero.scss";
+const GetHereHero = () => {
+  return (
+    <div className="get-here-hero">
+      <div className="get-here-hero-title">
+        <h1>GETTING HERE</h1>
+      </div>
+    </div>
+  );
+};
+
+export default GetHereHero;
