334567a57666d77c2964120388a5642cda4aef10
lolaaskerova <lolaaskrova2@gmail.com>
2023-02-13 12:39:45 +0400

    Gallery page: link field done and responsive

---
+++ frontend/src/components/GalleryComponents/GalleryLink/GalleryLink.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./galleryLink.scss";
+const GalleryLink = () => {
+  return (
+    <div className="gallery-link-field">
+      <h4>Would you like to share a photo? Follow us on Instagram #thrillz</h4>
+      <button>VIEW</button>
+    </div>
+  );
+};
+
+export default GalleryLink;
