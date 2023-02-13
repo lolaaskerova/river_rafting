1f239afc5b37c68001358a15c4088b13747fe052
lolaaskerova <lolaaskrova2@gmail.com>
2023-02-13 20:32:15 +0400

    Getting Here page: link part

---
+++ frontend/src/components/GettingHereComponents/GetHereLink/GetHereLink.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./gethereLink.scss";
+const GetHereLink = () => {
+  return (
+    <div className="get-here-link">
+      <h4>Don't yet have your tickets? Book yours today!</h4>
+      <button>BUY TICKETS</button>
+    </div>
+  );
+};
+
+export default GetHereLink;
