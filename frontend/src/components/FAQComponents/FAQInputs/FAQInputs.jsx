53cd1ed68ad050e531fe75f663a22ff6bd1aa9cf
lolaaskerova <lolaaskrova2@gmail.com>
2023-02-13 17:08:41 +0400

    FAQ page: inputs section done

---
+++ frontend/src/components/FAQComponents/FAQInputs/FAQInputs.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import QuestionInputs from "../../QuestionInputs/QuestionInputs";
+import "./faqInputs.scss";
+const FAQInputs = () => {
+  return (
+    <>
+      <div className="faq-input-title">
+        <h1>HAVE A QUESTION?</h1>
+      </div>
+      <QuestionInputs />
+    </>
+  );
+};
+
+export default FAQInputs;
