8452e916e00f880b70ebf946c030fa10044afd75
lolaaskerova <lolaaskrova2@gmail.com>
2023-02-13 15:53:38 +0400

    Tours page: reasons section updated

--- frontend/src/components/ToursComponents/ToursReasons/ToursReasons.jsx
+++ frontend/src/components/ToursComponents/ToursReasons/ToursReasons.jsx
@@ -3,6 +3,7 @@ import "./toursReasons.scss";
 import map from "../../../assets/images/map.png";
 import safety from "../../../assets/images/safety.png";
 import like from "../../../assets/images/like.png";
+import InfoContent from "../../InfoContent/InfoContent";
 const ToursReasons = () => {
   return (
     <div className="reasons container">
@@ -14,41 +15,27 @@ const ToursReasons = () => {
         </p>
       </div>
       <div className="tours-reasons">
-        <div className="reason">
-          <div className="icon">
-            <img src={safety} alt="reason" />
-          </div>
-          <div className="reason-titles">
-            <h4>Safety First</h4>
-            <p>
-              We are fully committed to your safety both on and off the river.
-            </p>
-          </div>
-        </div>
-        <div className="reason">
-          <div className="icon">
-            <img src={map} alt="reason" />
-          </div>
-          <div className="reason-titles">
-            <h4>World's Best Guides</h4>
-            <p>
-              Our guides are true whitewater professionals with Class 5
-              certifications.
-            </p>
-          </div>
-        </div>
-        <div className="reason">
-          <div className="icon">
-            <img src={like} alt="reason" />
-          </div>
-          <div className="reason-titles">
-            <h4>Experience of a Lifetime</h4>
-            <p>
-              We provide an adventure like nothing you've ever experienced
-              before.
-            </p>
-          </div>
-        </div>
+        <InfoContent
+          img={safety}
+          title={"Safety First"}
+          text={
+            " We are fully committed to your safety both on and off the river."
+          }
+        />
+        <InfoContent
+          img={map}
+          title={"World's Best Guides"}
+          text={
+            " Our guides are true whitewater professionals with Class 5 certifications."
+          }
+        />
+        <InfoContent
+          img={like}
+          title={"Experience of a Lifetime"}
+          text={
+            " We provide an adventure like nothing you've ever experienced before."
+          }
+        />
       </div>
     </div>
   );
