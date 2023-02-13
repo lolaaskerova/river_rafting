66982e7e7506a0a71c30c49eca9205fe274456fc
lolaaskerova <lolaaskrova2@gmail.com>
2023-02-13 18:57:55 +0400

    FAQ page done

--- frontend/src/pages/FAQ.jsx
+++ frontend/src/pages/FAQ.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import FAQEquipment from "../components/FAQComponents/FAQEquipment/FAQEquipment";
+import FAQHero from "../components/FAQComponents/FAQHero/FAQHero";
+import FAQInfo from "../components/FAQComponents/FAQInfo/FAQInfo";
+import FAQInputs from "../components/FAQComponents/FAQInputs/FAQInputs";
+import FAQPreparation from "../components/FAQComponents/FAQPreaparation/FAQPreparation";
+import FAQSafety from "../components/FAQComponents/FAQSafety/FAQSafety";
+import FAQStory from "../components/FAQComponents/FAQStory/FAQStory";
+import FAQSwiper from "../components/FAQComponents/FAQSwiper/FAQSwiper";
 const FAQ = () => {
   return (
     <>
       <Helmet>
         <title>FAQ - River Rafting</title>
       </Helmet>
+      <FAQHero />
+      <FAQInfo />
+      <FAQSafety />
+      <FAQSwiper />
+      <FAQPreparation />
+      <FAQStory />
+      <FAQEquipment />
+      <FAQInputs />
     </>
   );
 };
