e09984e3619af5fde78e753a88a33ea25a1d8b22
lolaaskerova <lolaaskrova2@gmail.com>
2023-02-14 19:21:35 +0400

    Careers page: perks section updated

--- frontend/src/components/CareersComponents/CareersPerks/CareersPerks.jsx
+++ frontend/src/components/CareersComponents/CareersPerks/CareersPerks.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./careersPerks.scss";
 import calendarIcon from "../../../assets/images/calendarIcon.png";
 import CareersPerksItem from "../CareersPerksItem/CareersPerksItem";
-
+import ball from "../../../assets/images/ball.png";
+import user from "../../../assets/images/user.png";
+import growth from "../../../assets/images/growth.png";
+import raft from "../../../assets/images/raft.png";
+import motor from "../../../assets/images/motor.png";
 const CareersPerks = () => {
   return (
     <div className="careers-perks-field">
@@ -20,31 +24,31 @@ const CareersPerks = () => {
                   paragraph="Cras nec consequat neque, nec placerat purus. Maecenas dui turpis, suscipit vel."
                 />
                 <CareersPerksItem
-                  img={calendarIcon}
+                  img={ball}
                   title="Fun atmosphere"
                   paragraph="Maecenas nibh ipsum, viverra faucibus consectetur non, maximus id ante."
                 />
               </div>
               <div className="careers-perk-items">
                 <CareersPerksItem
-                  img={calendarIcon}
+                  img={user}
                   title="Friendly team"
                   paragraph="Nullam faucibus pretium augue in iaculis. In leo dui, molestie vel congue in, viverra in."
                 />
                 <CareersPerksItem
-                  img={calendarIcon}
+                  img={growth}
                   title="Growth opportunity"
                   paragraph="Duis posuere hendrerit mi et tincidunt. Aliquam est est, rhoncus sit amet massa at, element."
                 />
               </div>
               <div className="careers-perk-items">
                 <CareersPerksItem
-                  img={calendarIcon}
+                  img={raft}
                   title="Free gear"
                   paragraph="Fusce in condimentum dolor. Donec quis pharetra nulla. Nam efficitur tortor sit amet."
                 />
                 <CareersPerksItem
-                  img={calendarIcon}
+                  img={motor}
                   title="Paid holidays"
                   paragraph="A lacinia elit, in malesuada orci. Nam imperdiet risus ut eleifend sodales. Phasellus dolor."
                 />
