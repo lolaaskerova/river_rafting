04576322f1e09c0ba5600ce7d73b7771f84ffb27
lolaaskerova <lolaaskrova2@gmail.com>
2023-02-15 17:57:50 +0400

    Shop page : Hero section done

---
+++ frontend/src/components/ShopComponents/ShopHero/ShopHero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./shopHero.scss";
+const ShopHero = () => {
+  return (
+    <section className="shop-hero">
+      <div className="shop-hero-title">
+        <h1>Shop</h1>
+      </div>
+    </section>
+  );
+};
+
+export default ShopHero;
