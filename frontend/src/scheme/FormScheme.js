ea1d56ef06e6064df940190bd7169bb95f0bdc50
lolaaskerova <lolaaskrova2@gmail.com>
2023-02-15 14:38:55 +0400

    yup

--- frontend/src/scheme/FormScheme.js
+++ frontend/src/scheme/FormScheme.js
@@ -1,7 +1,8 @@
 import * as yup from "yup";
 export const FormScheme = yup.object().shape({
-  name: yup.string().required("This field cannot be blankю"),
+  name: yup.string().required("This field cannot be blank."),
   email: yup.string().email().required("This field cannot be blank."),
   comment: yup.string().required("This field cannot be blank."),
   subject: yup.string().required("This field cannot be blank."),
+  message: yup.string().required("This field cannot be blank."),
 });
