1e2718304899f7a28289bde5ff5361266dc7b8d9
lolaaskerova <lolaaskrova2@gmail.com>
2023-02-16 12:06:18 +0400

    Cart page: hero section done

---
+++ frontend/src/components/BasketComponents/BasketHero/BasketHero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./basketHero.scss";
+const BasketHero = () => {
+  return (
+    <section className="basket-hero">
+      <div className="basket-hero-title">
+        <h1>CART</h1>
+      </div>
+    </section>
+  );
+};
+
+export default BasketHero;
