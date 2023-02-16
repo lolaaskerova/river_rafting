1b52db484afd05306e6bcbbb762df9f2d615b838
lolaaskerova <lolaaskrova2@gmail.com>
2023-02-16 12:13:52 +0400

    Check Out page: hero done

---
+++ frontend/src/components/CheckOutComponents/CheckOutHero/CheckOutHero.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import BasketHero from "../../BasketComponents/BasketHero/BasketHero";
+const CheckOutHero = () => {
+  return (
+    <>
+      <BasketHero />
+    </>
+  );
+};
+
+export default CheckOutHero;
