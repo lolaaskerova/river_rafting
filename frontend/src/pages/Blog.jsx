a26000a36d2b9b1c79722fc562b681e158c1281f
lolaaskerova <lolaaskrova2@gmail.com>
2023-02-18 20:13:58 +0400

    Blog page done

--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import BlogCards from "../components/BlogComponents/BlogCards/BlogCards";
+import { BlogHero } from "../components/BlogComponents/BlogHero/BlogHero";
 const Blog = () => {
   return (
     <>
       <Helmet>
         <title>Blog - River Rafting</title>
       </Helmet>
+      <BlogHero />
+      <BlogCards />
     </>
   );
 };
