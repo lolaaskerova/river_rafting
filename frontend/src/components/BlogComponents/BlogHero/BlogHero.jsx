47e5a58fa241af3564e3f71036fc642ec760ac39
lolaaskerova <lolaaskrova2@gmail.com>
2023-02-18 18:58:42 +0400

    Blog page:hero section done

---
+++ frontend/src/components/BlogComponents/BlogHero/BlogHero.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./blogHero.scss";
+export const BlogHero = () => {
+  return (
+    <section className="blog-hero">
+      <div className="blog-hero-title">
+        <h1>MASONRY BLOG</h1>
+      </div>
+    </section>
+  );
+};
