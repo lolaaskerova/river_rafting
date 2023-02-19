6222c93b741ad516d3eafce25d5388ef62acb46f
lolaaskerova <lolaaskrova2@gmail.com>
2023-02-19 13:28:31 +0400

    Tours

--- frontend/src/components/AllTours/AllTours.jsx
+++ frontend/src/components/AllTours/AllTours.jsx
@@ -1,23 +1,36 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./allTours.scss";
-import riverrock from "../../assets/images/riverrock.jpg";
-import waterfall from "../../assets/images/waterfall.jpg";
-import camping from "../../assets/images/camping.jpg";
-import AllToursItem from "../AllToursItem//AllToursItem";
-const AllTours = () => {
+
+const AllTours = ({ tours }) => {
   return (
-    <div className="row all-tours">
-      <AllToursItem
-        time={"6 hours"}
-        heading={"River Rock Tour"}
-        image={riverrock}
-      />
-      <AllToursItem time={"2 days"} heading={"Camping Tour"} image={camping} />
-      <AllToursItem
-        time={"8 hours"}
-        heading={"Waterfall Tour"}
-        image={waterfall}
-      />
+    <div className="all-tours">
+      <div className="row">
+        {tours &&
+          tours.map((t) => (
+            <div className="col-lg-4 col-md-6 col-sm-12 tour-card" key={t._id}>
+              <div className="tour-card-content">
+                {t.time >= 24 ? (
+                  <span>{t.time / 24} days</span>
+                ) : (
+                  <span>{t.time} hours</span>
+                )}
+                <div className="tour-card-title">
+                  <h3>{t.name}</h3>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  </p>
+                  <Link to="/tours">
+                    <button>VIEW TOUR</button>
+                  </Link>
+                </div>
+              </div>
+              <div className="tours-card-img">
+                <img src={t.image} alt="tourImg" />
+              </div>
+            </div>
+          ))}
+      </div>
     </div>
   );
 };
--- frontend/src/components/OurTours/OurTours.jsx
+++ frontend/src/components/OurTours/OurTours.jsx
@@ -1,8 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./ourTours.scss";
 import AllTours from "../AllTours/AllTours";
+import axios from "axios";
+import { useState } from "react";
 const OurTours = (props) => {
   const { title } = props;
+  const [sorted, setSorted] = useState({ sorted: "time", reversed: false });
+  const [tours, setTours] = useState([]);
+  const BASE_URL = "http://localhost:7777/tour";
+
+  const allData = async () => {
+    const res = await axios.get(BASE_URL);
+    setTours(res.data);
+  };
+
+  useEffect(() => {
+    allData();
+  }, []);
+  //sorting advanced
+  const sortAdvancedTours = async () => {
+    const advancedTours = tours.filter((t) => {
+      return t.time < 24;
+    });
+    setTours(advancedTours);
+  };
+
+  //sorting 'multi days'
+  const sortTours = async () => {
+    const sortedTours = tours.filter((t) => {
+      return t.time > 24;
+    });
+    setTours(sortedTours);
+  };
+
+  // sorting all and rapids
+  const sortAllTours = () => {
+    allData();
+  };
+
   return (
     <div className="our-tours-part">
       <div className="tours-container">
@@ -20,12 +55,12 @@ const OurTours = (props) => {
           <div className="tours-content-bottom">
             <div className="our-tours-sorting">
               <span>Sort :</span>
-              <button>All</button>
-              <button>Advanced</button>
-              <button>Multi-Day</button>
-              <button>Rapids</button>
+              <button onClick={() => sortAllTours()}>All</button>
+              <button onClick={() => sortAdvancedTours()}>Advanced</button>
+              <button onClick={() => sortTours()}>Multi - Day</button>
+              <button onClick={() => sortAllTours()}>Rapids</button>
             </div>
-            <AllTours />
+            <AllTours tours={tours} />
           </div>
         </div>
       </div>
